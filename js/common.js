685185a5a95116c6d694c0901b38589c9b37d09d
bogdan <butymbogdan@gmail.com>
2017-01-22 21:07:34 +0200

    анімація

--- js/common.js
+++ js/common.js
@@ -3,4 +3,35 @@ $(document).ready(function(){
     var heightR = $('.box-offers').height();
 
     $('.box-offers .item-section').css({'height':heightR});
+
+    $('.item-section2,.item-section3,.item-section4,.item1,.item2,.item3,.item4,.item5').viewportChecker({
+        classToAdd: 'visible animated fadeIn',
+        repeat:true,
+        offset:10
+    });
+   $('.number').viewportChecker({
+        callbackFunction: function(){
+            $('.number-1').animate({ num: 5200}, {
+                duration: 3000,
+                step: function (num){
+                    this.innerHTML = (num).toFixed(0)
+                }
+            });
+
+            $('.number-2').animate({ num: 14}, {
+                duration: 3000,
+                step: function (num){
+                    this.innerHTML = (num).toFixed(0)
+                }
+            });
+
+            $('.number-3').animate({ num: 3.2}, {
+                duration: 3000,
+                step: function (num){
+                    this.innerHTML = (num).toFixed(1)+'<br><small>млн</small>'
+                }
+            });
+        },
+        repeat:true
+    });
 });
