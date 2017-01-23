f062cb66acb65b2dfe7473f88472d2c1a6995e26
bogdan <butymbogdan@gmail.com>
2017-01-23 22:17:14 +0200

    +++

--- js/common.js
+++ js/common.js
@@ -1,9 +1,5 @@
 $(document).ready(function(){
 
-    var heightR = $('.box-offers').height();
-
-    $('.box-offers .item-section').css({'height':heightR});
-
     $('.item-section1,.item-section2,.item-section3,.item-section4,.item1,.item2,.item3,.item4,.item5').viewportChecker({
         classToAdd: 'visible animated fadeIn',
         repeat:true,
@@ -34,4 +30,7 @@ $(document).ready(function(){
         },
         repeat:true
     });
+            $("a#df").click(function(){$.scrollTo('section.footer', {duration:1500});
+                return false;});
+
 });
